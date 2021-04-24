74263de35848964ee0b50087aec248aaf43dc196
Aidan Christopher <gandamovies@gmail.com>
2021-04-24 02:46:41 -0700

    done registermodel, working through registermanager

---
+++ register manager.py
@@ -0,0 +1,165 @@
+from registermodel import Register
+from random import uniform
+from random import randint
+import math
+import json
+
+class RegisterManager():
+    def __init__(self):
+        self._registers = []
+    
+    @property
+    def money(self):
+        if len(self._registers) == 0:
+            return "There are no registers tied to the manager"
+        else:
+            money = 0
+            for reg in self._registers:
+                money = money + reg.get_total_made
+            return "${:.2f}".format(money)
+    
+    def add_register(self,register):
+        if not (register, Register):
+            print("This is not a Register obj")
+        else:
+            add = True
+            for reg in self._registers:
+                if reg.id == register.id:
+                    print("There is already a register under this ID")
+                    add = False
+                    break
+            if add:
+                self._registers.append(register)
+    
+    def remove_register(self,register):
+        if not (register, Register):
+            print("This is not a Register obj")
+        else:
+            drem = True
+            for reg in self._registers:
+                if reg.id == register.id:
+                    self._registers.remove(register)
+                    drem = False
+                    break
+            if drem:
+                print("There is no register under this ID")
+    
+    def print_registers(self):
+        if len(self._registers) == 0:
+            print("There are no registers tied to the manager")
+        else:
+            registerids = []
+            for reg in self._registers:
+                registerids.append(reg.id)
+            print("Registers: ",end='')
+            print(', '.join(map(str,registerids)))
+    
+    def register_rundown(self,id):
+        if len(self._registers) == 0:
+            print("There are no registers tied to the manager")
+        else:
+            drd = True
+            for reg in self._registers:
+                if reg.id == id:
+                    print("REGISTER {}:".format(id))
+                    reg.rundown()
+                    drd = False
+                    break
+            if drd:
+                print("There is no register under this ID")
+    
+    def rundown(self):
+        if len(self._registers) == 0:
+            return "There are no registers tied to the manager"
+        else:
+            gettotalmade = 0
+            cashmoneymade = 0
+            debitmoneymade = 0
+            creditmoneymade = 0
+            giftcardmoneymade = 0
+            moneyreturned = 0
+            totaltrans = 0
+            cashtrans = 0
+            debittrans = 0
+            credittrans = 0
+            giftcardtrans = 0
+            returntrans = 0
+            itemspertrans = 0
+            totalpurchasetrans = 0
+            moneymade = 0
+            for reg in self._registers:
+                gettotalmade += reg.get_total_made
+                cashmoneymade += reg._cash_money_made
+                debitmoneymade += reg._debit_money_made
+                creditmoneymade += reg._credit_money_made
+                giftcardmoneymade += reg._giftcard_money_made
+                moneyreturned += reg._money_returned
+                totaltrans += reg.get_total_trans
+                cashtrans += reg._cash_trans
+                debittrans += reg._debit_trans
+                credittrans += reg._credit_trans
+                giftcardtrans += reg._giftcard_trans
+                returntrans += reg._return_trans
+                itemspertrans += reg._items_pertrans
+                totalpurchasetrans += reg.get_total_purchase_trans
+                moneymade += reg._money_made
+
+
+            print('Total registers: {}'.format(len(self._registers)))
+            print('Total money made: ${:.2f}'.format(gettotalmade))
+            print('Money made by cash: ${:.2f}\nMoney made by debit: ${:.2f}\nMoney made by credit: ${:.2f}\nMoney made by giftcards: ${:.2f}'.format(cashmoneymade,debitmoneymade,creditmoneymade,giftcardmoneymade))
+            print('Money returned: ${:.2f}'.format(moneyreturned))
+            print('\nTotal transactions: {}\nNumber of cash transactions:{}\nNumber of debit transactions:{}\nNumber of credit transactions:{}\nNumber of giftcards transactions:{}\nNumber of return transactions:{}'.format(totaltrans,cashtrans,debittrans,credittrans,giftcardtrans,returntrans))
+            print('\nCash in registers:')
+            for reg in self._registers:
+                print('Register {} - ${:.2f}'.format(reg.id,reg._cashinregister))
+            print('\nItems purchased: {}\nAverage number of items per transaction: {:.2f}\nAverage cost per item: ${:.2f}\nAverage transaction cost: ${:.2f}'.format(itemspertrans,itemspertrans/totalpurchasetrans,moneymade/itemspertrans,moneymade/totalpurchasetrans))
+  
+
+
+def fill(regi):
+    def grp():
+        return round(uniform(40,100),2)
+    def gri():
+        return round(uniform(1,10))
+    def grt():
+        return round(uniform(50,200))
+
+    def group_cash(reg):
+        for i in range(grt()):
+            price = grp()
+            reg.cash_transaction(price,math.ceil(price),gri())
+    def group_debit(reg):
+        for i in range(grt()):
+            reg.debit_transaction(grp(),gri())
+    def group_credit(reg):
+        for i in range(grt()):
+            reg.credit_transaction(grp(),gri())
+    def group_giftcard(reg):
+        for i in range(grt()):
+            reg.giftcard_transaction(grp(),gri())
+    def group_return(reg):
+        methods = ['cash','credit','debit','giftcard']
+        for i in range(grt()):
+            method_i = randint(0,3)
+            reg.return_item(methods[method_i],grp()/2)
+    group_cash(regi)
+    group_credit(regi)
+    group_debit(regi)
+    group_giftcard(regi)
+    group_return(regi)
+
+if __name__=="__main__":
+    rm = RegisterManager()
+    reg1 = Register(1,250)
+    reg2 = Register(2,250)
+    reg3 = Register(3,250)
+
+    fill(reg1)
+    fill(reg2)
+    fill(reg3)
+
+    rm.add_register(reg1)
+    rm.add_register(reg2)
+    rm.add_register(reg3)
+    rm.rundown()
---
+++ registermodel.py
@@ -0,0 +1,163 @@
+from random import uniform
+from random import randint
+import math
+import json
+
+class Register():
+    def __init__(self,id,startingcash):
+        if isinstance(id, bool):
+            raise TypeError
+        if not isinstance(id, int):
+            raise TypeError
+        if isinstance(float(startingcash), bool):
+            raise TypeError
+        if not isinstance(float(startingcash), float):
+            raise TypeError
+    
+        self._id = id
+
+        self._startingcash = float(startingcash)
+        self._cashinregister = float(startingcash)
+
+        self._money_made = 0.0
+        self._money_returned = 0.0
+        self._return_trans = 0
+
+        self._items_pertrans = 0
+
+        self._cash_trans = 0
+        self._cash_money_made = 0.0
+
+        self._debit_trans = 0
+        self._debit_money_made = 0.0
+
+        self._credit_trans = 0
+        self._credit_money_made = 0.0
+
+        self._giftcard_trans = 0
+        self._giftcard_money_made = 0.0
+    
+    @property
+    def get_total_made(self):
+        return self._money_made-self._money_returned
+    @property
+    def get_total_trans(self):
+        return self._return_trans+self._debit_trans+self._cash_trans+self._credit_trans+self._giftcard_trans
+    @property
+    def get_total_purchase_trans(self):
+        return self._debit_trans+self._cash_trans+self._credit_trans+self._giftcard_trans
+    @property
+    def id(self):
+        return self._id
+
+    def cash_transaction(self,price,cashgiven,items):
+        self._money_made = self._money_made + price
+        self._cashinregister = self._cashinregister + price
+        self._cash_money_made = self._cash_money_made + price
+        self._cash_trans += 1
+        self._items_pertrans = self._items_pertrans + items
+        #print("Change for customer: {:.2f}".format(cashgiven-price))
+    
+    def debit_transaction(self,price,items):
+        self._money_made = self._money_made + price
+        self._debit_money_made = self._debit_money_made + price
+        self._debit_trans += 1
+        self._items_pertrans = self._items_pertrans + items
+    
+    def credit_transaction(self,price,items):
+        self._money_made = self._money_made + price
+        self._credit_money_made = self._credit_money_made + price
+        self._credit_trans += 1
+        self._items_pertrans = self._items_pertrans + items
+    
+    def giftcard_transaction(self,price,items):
+        self._money_made = self._money_made + price
+        self._giftcard_money_made = self._giftcard_money_made + price
+        self._giftcard_trans += 1
+        self._items_pertrans = self._items_pertrans + items
+
+    def return_item(self,method,rvalue):
+        cont = True
+        if method == 'cash':
+            if rvalue > self._cashinregister:
+                cont = False
+            else:
+                self._cashinregister = self._cashinregister - rvalue
+        if cont:
+            self._money_returned = self._money_returned + rvalue
+            self._return_trans += 1
+        else:
+            print('Not enough money in the register!')
+
+    
+    def rundown(self):
+        print('Total money made: ${:.2f}'.format(self.get_total_made))
+        print('Money made by cash: ${:.2f}\nMoney made by debit: ${:.2f}\nMoney made by credit: ${:.2f}\nMoney made by giftcards: ${:.2f}'.format(self._cash_money_made,self._debit_money_made,self._credit_money_made,self._giftcard_money_made))
+        print('Money returned: ${:.2f}'.format(self._money_returned))
+        print('\nTotal transactions: {}\nNumber of cash transactions:{}\nNumber of debit transactions:{}\nNumber of credit transactions:{}\nNumber of giftcards transactions:{}\nNumber of return transactions:{}'.format(self.get_total_trans,self._cash_trans,self._debit_trans,self._credit_trans,self._giftcard_trans,self._return_trans))
+        print('\nCash in register: ${:.2f}'.format(self._cashinregister))
+        print('\nItems purchased: {}\nAverage number of items per transaction: {:.2f}\nAverage cost per item: ${:.2f}\nAverage transaction cost: ${:.2f}'.format(self._items_pertrans,self._items_pertrans/self.get_total_purchase_trans,self._money_made/self._items_pertrans,self._money_made/self.get_total_purchase_trans))
+    
+    def rundown_json(self):
+        json_export = {
+            'money_details':{
+                'total_money_made':round(self.get_total_made,2),
+                'cash':round(self._cash_money_made,2),
+                'debit':round(self._debit_money_made,2),
+                'credit':round(self._credit_money_made,2),
+                'giftcard':round(self._giftcard_money_made,2),
+                'returned':round(self._money_returned,2)
+            },
+            'transaction_details':{
+                'total_transactions':self.get_total_trans,
+                'cash':self._cash_trans,
+                'debit':self._debit_trans,
+                'credit':self._credit_trans,
+                'giftcard':self._giftcard_trans,
+                'return':self._return_trans,
+            },
+            'cash_in_register':round(self._cashinregister,2),
+            'stats':{
+                'items_purchased':self._items_pertrans,
+                'avg_item_per_trans':round(self._items_pertrans/self.get_total_purchase_trans,2),
+                'avg_cost_per_item':round(self._money_made/self._items_pertrans,2),
+                'avg_trans_cost':round(self._money_made/self.get_total_purchase_trans,2)
+            }
+        }
+        with open('register_'+str(self._id)+'_rundow.json', 'w') as file:
+            json.dump(json_export, file)
+
+if __name__=='__main__':
+    def grp():
+        return round(uniform(40,100),2)
+    def gri():
+        return round(uniform(1,10))
+    def grt():
+        return round(uniform(50,200))
+
+    def group_cash(reg):
+        for i in range(grt()):
+            price = grp()
+            reg.cash_transaction(price,math.ceil(price),gri())
+    def group_debit(reg):
+        for i in range(grt()):
+            reg.debit_transaction(grp(),gri())
+    def group_credit(reg):
+        for i in range(grt()):
+            reg.credit_transaction(grp(),gri())
+    def group_giftcard(reg):
+        for i in range(grt()):
+            reg.giftcard_transaction(grp(),gri())
+    def group_return(reg):
+        methods = ['cash','credit','debit','giftcard']
+        for i in range(grt()):
+            method_i = randint(0,3)
+            reg.return_item(methods[method_i],grp()/2)
+    x = Register(1,250)
+    group_cash(x)
+    group_credit(x)
+    group_debit(x)
+    group_giftcard(x)
+    group_return(x)
+    x.rundown()
+    x.rundown_json()
